2cc01599eb674dce4d2400304e925cf33b3534f4
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-23 15:31:12 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,9 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
-  -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -Version "1.0.0" `
+  -OutputFilePrefix "DBSource_0.1.4" `
+  -ModName "Dragon Ball Source 0.1.4" `
+  -ModFolder "DBSource" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/dragon-ball-source/DBSource-0.1.4-Client-Original.zip" `
+  -ModBaseFilesUrlHash "d73a2253c768dbf4c5a99d8b7beed0d4d564439ce5fbca24490ecfa491e2ad60" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
